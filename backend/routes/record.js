9855da726f188c4f2bc5938fe02608ebd6d19ae0
KhengTang <khengtangjack@gmail.com>
2022-08-27 15:15:13 +0800

    Update db id

--- backend/routes/record.js
+++ backend/routes/record.js
@@ -14,7 +14,7 @@ const ObjectId = require("mongodb").ObjectId;
  
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req, res) {
- let db_connect = dbo.getDb("employees");
+ let db_connect = dbo.getDb("users");
  db_connect
    .collection("records")
    .find({})
@@ -40,9 +40,8 @@ recordRoutes.route("/record/:id").get(function (req, res) {
 recordRoutes.route("/record/add").post(function (req, response) {
  let db_connect = dbo.getDb();
  let myobj = {
-   name: req.body.name,
-   position: req.body.position,
-   level: req.body.level,
+   email: req.body.email,
+   password: req.body.password,
  };
  db_connect.collection("records").insertOne(myobj, function (err, res) {
    if (err) throw err;
@@ -56,9 +55,8 @@ recordRoutes.route("/update/:id").post(function (req, response) {
  let myquery = { _id: ObjectId(req.params.id) };
  let newvalues = {
    $set: {
-     name: req.body.name,
-     position: req.body.position,
-     level: req.body.level,
+    email: req.body.email,
+    password: req.body.password,
    },
  };
  db_connect
